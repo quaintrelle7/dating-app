d13a75547f6b973c61f28349746b6e5f249725dc
quaintrelle7 <sharayushendre7@gmail.com>
2022-08-09 22:37:20 +0530

    first commit

---
+++ src/components/SwipeButtons.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SwipeButtons.css'
+
+const SwipeButtons = () => {
+    return (
+        <div className="swipeButtons">
+
+        </div>
+    )
+}
+
+export default SwipeButtons
